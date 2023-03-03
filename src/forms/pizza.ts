75cd76ca620e7b8cbf48d7b954cdccc6ba1e3c7e
Gui Seek <guiseek@gmail.com>
2023-03-02 22:14:53 -0300

    pizza form

---
+++ src/forms/pizza.ts
@@ -0,0 +1,25 @@
+export const initPizzaForm = () => {
+  
+  
+  const form = document.querySelector('form')
+
+  if (form) {
+    
+    const custname = form.querySelector<HTMLInputElement>('#custname')
+    
+    custname.oninput = () => {
+      console.log(custname.validity.tooShort);
+      
+    }
+    
+
+    form.onsubmit = (ev) => {
+      ev.preventDefault()
+
+      const data = new FormData(form)
+      console.log(data.getAll('topping'));
+      
+      console.log(Object.fromEntries(data.entries()))
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,6 @@
 import './style.scss'
+import html from './forms/pizza.html?raw'
+
+const app = document.querySelector('main')
+app.innerHTML = html
 
