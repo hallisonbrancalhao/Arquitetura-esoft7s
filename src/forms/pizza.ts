a4038a8e53229e825cd3dd320adbfd0347936610
Hallison <hallison.o.b@gmail.com>
2023-03-27 18:16:29 -0300

    aula24032023

--- src/forms/pizza.ts
+++ src/forms/pizza.ts
@@ -1,25 +1,20 @@
 export const initPizzaForm = () => {
-  
-  
   const form = document.querySelector('form')
 
   if (form) {
-    
     const custname = form.querySelector<HTMLInputElement>('#custname')
-    
+
     custname.oninput = () => {
-      console.log(custname.validity.tooShort);
-      
+      console.log(custname.validity.tooShort)
     }
-    
 
     form.onsubmit = (ev) => {
       ev.preventDefault()
 
       const data = new FormData(form)
-      console.log(data.getAll('topping'));
-      
-      console.log(Object.fromEntries(data.entries()))
+      console.log(data.getAll('topping'))
+
+      console.log(Object.fromEntries(data.getAll()))
     }
   }
 }
